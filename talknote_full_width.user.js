7b3619935a71e01aad237d4423e32869b497b352
oe-mediba <y-oe@mediba.jp>
2013-12-08 23:57:25 +0900

    impl notification

--- talknote_full_width.user.js
+++ talknote_full_width.user.js
@@ -11,7 +11,9 @@
 function addGlobalStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
-    if (!head) { return; }
+    if (!head) {
+        return;
+    }
     style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = css;
@@ -37,31 +39,44 @@ addGlobalStyle('\
 
 // jQuery使いたいけど chrome拡張機能へ D&D install する場合 require 使えないので
 // http://stackoverflow.com/questions/2246901/how-can-i-use-jquery-in-greasemonkey-scripts-in-google-chrome
-var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
+var load, execute, loadAndExecute;
+load = function(a, b, c) {
+    var d;
+    d = document.createElement("script"), d.setAttribute("src", a), b != null && d.addEventListener("load", b), c != null && d.addEventListener("error", c), document.body.appendChild(d);
+    return d
+}, execute = function(a) {
+    var b, c;
+    typeof a == "function" ? b = "(" + a + ")();" : b = a, c = document.createElement("script"), c.textContent = b, document.body.appendChild(c);
+    return c
+}, loadAndExecute = function(a, b) {
+    return load(a, function() {
+        return execute(b)
+    })
+};
 
 loadAndExecute("//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js", function() {
 
     // マウスエンターでmessage_container表示
     var mo_timeout = false;
-    $('#contents').on('mouseenter', 'li.status', function(event){
+    $('#contents').on('mouseenter', 'li.status', function(event) {
         var $status = $(this);
         mo_timeout = setTimeout(function() {
             $status.find('.message_container').slideDown('slow');
         }, 1000);
     });
-    $('#contents').on('mouseleave', 'li.status', function(event){
+    $('#contents').on('mouseleave', 'li.status', function(event) {
         if (mo_timeout !== false) {
             clearTimeout(mo_timeout);
-        	mo_timeout = false;
+            mo_timeout = false;
         }
         //$(this).find('.message_container').slideUp('slow');
     });
     // クリックでmessage_container表示On/Off
-    $('#contents').on('click', 'li.status:not(a)', function(event){
+    $('#contents').on('click', 'li.status:not(a)', function(event) {
         console.log('e:' + event.target.id);
         console.log('s:' + $(this).attr('id'));
         if (event.target.id === $(this).attr('id')) {
-        	$(this).find('.message_container').toggle();
+            $(this).find('.message_container').toggle();
         }
     });
 
@@ -70,17 +85,17 @@ loadAndExecute("//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js", fun
         // 全体の横幅を100%に
         $('#wrapper').css({'width': width + 'px'});
         // コンテナーの横幅を(全体-左ナビ)に
-        $('#container').css({'width': (width - 250 - 20)+ 'px'});
+        $('#container').css({'width': (width - 250 - 20) + 'px'});
         // コンテンツの横幅を(全体-左ナビ-マージン)に
-        $('#contents').css({'width': (width - 250 - 20 - 30)+ 'px'});
+        $('#contents').css({'width': (width - 250 - 20 - 30) + 'px'});
     };
     // 左ナビの横幅を拡大
     $('#left_nav').css({'width': '250px'});
     resize();
-    
+
     // 右ナビを左ナビ下に移動
     $('#right_nav').appendTo('#left_nav');
-    
+
     // 画面リサイズしたら再リサイズ
     var timer = false;
     $(window).resize(function() {
@@ -91,43 +106,78 @@ loadAndExecute("//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js", fun
             resize();
         }, 200);
     });
-    
+
+    // 検索保存
     var keywords = [];
     if (localStorage.keywords) {
-    	keywords = JSON.parse(localStorage.getItem('keywords'));
+        keywords = JSON.parse(localStorage.getItem('keywords'));
     } else {
-        keywords = {'リリース':1, 'テスト':1};
-    	localStorage.setItem('keywords', JSON.stringify(keywords));
+        keywords = {'リリース': 1, 'テスト': 1};
+        localStorage.setItem('keywords', JSON.stringify(keywords));
     }
-    
+
     $keywords = $('<ul>');
     for (var keyword in keywords) {
         var $keyword = $('<li id="keyword_' + keyword + '" class="keyword">').append('<a href="/mediba.jp/search/' + encodeURIComponent(keyword) + '/">' + keyword + '</a><button class="rmKeyword" data-keyword="' + keyword + '">－</button>');
         $keywords.append($keyword);
-    };
+    }
+    ;
     $keywords.append($('<li class="newKeyword"><input type="text" id="newKeyword"><button id="addKeyword">＋</button></li>'));
     $storedSearch = $('<ul>').append($('<li>').append('<a>検索</a>').append($keywords));
     $('.left_menu').append($storedSearch);
-    
-    $('#addKeyword').on('click', function () {
-    	var keyword = $('#newKeyword').val();
+
+    $('#addKeyword').on('click', function() {
+        var keyword = $('#newKeyword').val();
         $('#newKeyword').val('');
         keywords = JSON.parse(localStorage.getItem('keywords'));
         keywords[keyword] = 1;
-    	localStorage.setItem('keywords', JSON.stringify(keywords));
-        
+        localStorage.setItem('keywords', JSON.stringify(keywords));
+
         var $keyword = $('<li id="keyword_' + keyword + '" class="keyword">').append('<a href="/mediba.jp/search/' + encodeURIComponent(keyword) + '/">' + keyword + '</a><button class="rmKeyword" data-keyword="' + keyword + '">－</button>');
         $(this).parent().before($keyword);
         console.log(keyword + ' add');
     });
-    $('.left_menu').on('click', 'button.rmKeyword', function () {
+    $('.left_menu').on('click', 'button.rmKeyword', function() {
         var keyword = $(this).attr('data-keyword');
         var keywords = JSON.parse(localStorage.getItem('keywords'));
         delete keywords[keyword];
-    	localStorage.setItem('keywords', JSON.stringify(keywords));
-        
+        localStorage.setItem('keywords', JSON.stringify(keywords));
+
         $(this).parent().remove();
         console.log(keyword + ' remove');
     });
-    
+
+    // 通知
+    var prelink = '';
+    $('#news_feed li').trigger('click');
+    setInterval(function() {
+        var $first_new = $('#new_at_header a:first');
+        var link = $first_new.attr('href');
+        if (prelink && prelink != link) {
+            var icon = $first_new.find('img').attr('src');
+            var body = $first_new.find('.message').text().trim() + "\n" + $first_new.find('.time').text().trim();
+            console.log(link, icon, body);
+            notify(icon, body, link);
+            prelink = link;
+        }
+    }, 1000 * 60 * 5); // 5min
+    function notify(icon, body, link) {
+        var havePermission = window.webkitNotifications.checkPermission();
+        if (havePermission == 0) {
+            // 0 is PERMISSION_ALLOWED
+            var notification = window.webkitNotifications.createNotification(
+                    icon,
+                    "Talknote",
+                    body
+                    );
+            notification.onclick = function() {
+                window.focus();
+                this.cancel();
+            }
+            notification.show();
+        } else {
+            window.webkitNotifications.requestPermission();
+        }
+    }
+
 });
