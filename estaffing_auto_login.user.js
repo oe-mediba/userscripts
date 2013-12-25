48a0a7ce9d4505a4e923c03b98ac43d52b19c808
oe-mediba <y-oe@mediba.jp>
2013-12-25 11:16:50 +0900

    勤怠入力画面まで遷移して、定時ボタン自動クリック

--- estaffing_auto_login.user.js
+++ estaffing_auto_login.user.js
@@ -1,16 +1,22 @@
 // ==UserScript==
 // @name       e-staffing auto login
-// @namespace  http://use.i.E.your.homepage/
-// @version    0.1
+// @namespace  https://github.com/oe-mediba/userscripts
+// @version    0.2
 // @description  e-staffing auto login
-// @match      https://timecard2.e-staffing.ne.jp/000_002.cfm
-// @copyright  2012+, You
+// @match      https://timecard2.e-staffing.ne.jp/*
+// @copyright  2013+, You
 // @require http://code.jquery.com/jquery-2.0.3.min.js
 // ==/UserScript==
 
 (function ($) {
-    $('form input:eq(0)').val('hoge'); // 企業ID
-    $('form input:eq(1)').val('foo'); // ユーザID
-    $('form input:eq(2)').val('bar'); // パスワード
-    Login(1)
+    if (location.href == 'https://timecard2.e-staffing.ne.jp/000_002.cfm') {
+        $('form input:eq(0)').val('foo'); // 企業ID
+        $('form input:eq(1)').val('bar'); // ユーザID
+        $('form input:eq(2)').val('hoge'); // パスワード
+        Login(1);
+    }
+    if (location.href == 'https://timecard2.e-staffing.ne.jp/staff/001_003.cfm') {
+    	MovePage(0);
+    }
+    $("input[type='button'][value='定時']").trigger('click');
 })(jQuery);
