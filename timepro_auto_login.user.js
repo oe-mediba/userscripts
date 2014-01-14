dbec0b19b1cdbfe1bd693bb2ce47644e9d6938da
Hiroki Yoshida <h-yoshida@mediba.jp>
2014-01-14 18:58:52 +0900

    TimePro-XG

---
+++ timepro_auto_login.user.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name       timepro auto login
+// @namespace  https://github.com/yoshida-mediba/userscripts
+// @version    0.1
+// @description  e-staffing auto login
+// @match      http://192.168.1.58/*
+// @copyright  2013+, You
+// @require http://code.jquery.com/jquery-2.0.3.min.js
+// ==/UserScript==
+
+(function ($) {
+
+    // パスワード登録
+    if (location.href == 'http://192.168.1.58/XGweb/login.asp') {
+        document.forms.item(0).LoginID.value = 'foo'; // ユーザID
+        document.forms.item(0).PassWord.value = 'bar'; // パスワード
+        NewPassWordEnter(document.frmLOGIN);
+    }
+
+    // そしてログイン
+    if (location.href == 'http://192.168.1.58/XGweb/Login.asp') {
+        document.forms.item(0).NewPassWord.value = 'bar'; // 新パスワード
+        document.forms.item(0).ConfirmPassWord.value = 'bar'; // (再入力)
+        PassWordRegister(document.frmLOGIN);
+    }
+
+    /* 状態を保持してなんとかかんとかやれたらいいな (つまりやらない) */
+    // 出勤
+    // if (location.href == 'http://192.168.1.58/XGweb/frame.asp') {
+    //     frameBtm.PanchWindowOpen(1);
+    // }
+    // 退勤
+    // if (location.href == 'http://192.168.1.58/XGweb/frame.asp') {
+    //     frameBtm.PanchWindowOpen(1);
+    // }
+})(jQuery);
