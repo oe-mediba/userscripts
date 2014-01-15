4b12376addf72c1492e84e7d5477e81de34d1a34
Hiroki Yoshida <h-yoshida@mediba.jp>
2014-01-15 10:20:28 +0900

    logout page

--- timepro_auto_login.user.js
+++ timepro_auto_login.user.js
@@ -9,23 +9,29 @@
 // ==/UserScript==
 
 (function ($) {
+
     // パスワード登録
     if (location.href == 'http://192.168.1.58/XGweb/login.asp') {
-        document.forms.item(0).LoginID.value = 'foo'; // ユーザID
-        document.forms.item(0).PassWord.value = 'bar'; // パスワード
+        document.forms.item(0).LoginID.value = 'user'; // ユーザID
+        document.forms.item(0).PassWord.value = 'pass'; // パスワード
         NewPassWordEnter(document.frmLOGIN);
     }
 
     // そしてログイン
     if (location.href == 'http://192.168.1.58/XGweb/Login.asp') {
-        document.forms.item(0).NewPassWord.value = 'bar'; // 新パスワード
-        document.forms.item(0).ConfirmPassWord.value = 'bar'; // (再入力)
+        document.forms.item(0).NewPassWord.value = 'pass'; // 新パスワード
+        document.forms.item(0).ConfirmPassWord.value = 'pass'; // (再入力)
         PassWordRegister(document.frmLOGIN);
     }
 
     // 出勤か退勤が押されたら戻る
     if (location.href == 'http://192.168.1.58/XGweb/page/XgwTopMenu.asp#') {
         history.back();
+    }
+
+    // ログアウトした時に再突入しないようにページ書き変え
+    if(location.href == 'http://192.168.1.58/XGweb/page/XgwCheckSession.htm') {
+        document.write('ログアウト完了');
     }
 
     /* 状態を保持してなんとかかんとかやれたらいいな (つまりやらない) */
